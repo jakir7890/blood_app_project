bb9542804eea4463889352274133fa373a825fa6
Syed Jakir <106330871+jakir7890@users.noreply.github.com>
2022-08-08 16:40:15 +0600

    scroll

--- lib/screen/User_login/user_login.dart
+++ lib/screen/User_login/user_login.dart
@@ -2,65 +2,45 @@ import 'package:blood_app/screen/User_login/donner.dart';
 import 'package:blood_app/screen/User_login/nedded.dart';
 import 'package:flutter/material.dart';
 
-class UserLoginScreen extends StatelessWidget {
+class UserLoginScreen extends StatefulWidget {
   const UserLoginScreen({Key? key}) : super(key: key);
 
   @override
+  State<UserLoginScreen> createState() => _UserLoginScreenState();
+}
+
+class _UserLoginScreenState extends State<UserLoginScreen>
+    with TickerProviderStateMixin {
+  TabController? _tabController;
+  @override
+  void initState() {
+    _tabController = TabController(length: 2, vsync: this);
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return  DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        
-        appBar: AppBar(
-          title: Text('Sign Up'),
-          centerTitle: true,
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            
-            children: [SizedBox(height: 20,),
-               Padding(
-                 padding:  EdgeInsets.only(left: 15,right: 15),
-                 child: TabBar(
-                     indicator: BoxDecoration(
-                      borderRadius: BorderRadius.circular(50),
-                      color: Colors.green),
-                   unselectedLabelColor: Colors.black54,
-                 
-                 labelColor: Colors.white,
-                 
-                 indicatorColor: Colors.white,
-                   tabs: [
-                     Tab(
-                       text: "Blood Donner",),
-                   
-                      Tab(
-                       text: "Blood Needed"
-                  
-                     ),
-                     
-                     
-                     
-                   ],
-                   
-                   ),
-               ),
-              Container(
-                height: 750,
-                child:   TabBarView(
-                  children: [
-                    BloodDonner(),
-                    BloodNeeded()
-                
-                  
-                ]
-                ),
-              )
-            ],
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sign Up'),
+        centerTitle: true,
+        bottom: TabBar(
+          controller: _tabController,
+          indicator: BoxDecoration(
+              borderRadius: BorderRadius.circular(50), color: Colors.green),
+          unselectedLabelColor: Colors.black54,
+          labelColor: Colors.white,
+          indicatorColor: Colors.white,
+          tabs: [
+            Tab(
+              text: "Blood Donner",
+            ),
+            Tab(text: "Blood Needed"),
+          ],
         ),
       ),
+      body: TabBarView(
+          controller: _tabController, children: [BloodDonner(), BloodNeeded()]),
     );
   }
 }
-
